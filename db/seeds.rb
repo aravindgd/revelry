c7f6d563f0af4a8ac8d7cd78b6652a7f72577f63
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2016-10-20 11:19:51 +0530

    small change in seeds to reflect name difference for understanding the diff between one article and naother

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 owner = Owner.create(name: "Matz")
 10.times do |iter1|
-  owner.articles.build(name: "MINASWAN", price: rand(1.0..100.1), description: 'some desc').save!
+  owner.articles.build(name: "MINASWAN#{iter1}", price: rand(1.0..100.1), description: 'some desc').save!
 end
 10.times do |iter|
   owner = Owner.create!(name: "owner#{iter}")
